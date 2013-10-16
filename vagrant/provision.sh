8533f574c254b7559d2a4d1f5b5c7b0703ce24b7
Phillip Shipley <phillip_shipley@sil.org>
2013-10-16 16:49:58 -0400

    updated vagrant config

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -7,10 +7,11 @@ sudo apt-get install -y git php5
 sudo mount -t vboxsf -o uid=`id -u www-data`,gid=`id -g www-data` /var/www/swank /var/www/swank
 
 # Make sure the timezone is set in php.ini.
-sudo sed -i".bak" "s/^\;date\.timezone.*$/date\.timezone = \"America\\/New_York\" /g" /etc/php.ini
+sudo sed -i".bak" "s/^\;date\.timezone.*$/date\.timezone = \"America\\/New_York\" /g" /etc/php5/apache2/php.ini
 
 # Change apache servername
-sudo sed -i "s/#ServerName www.example.com:80/ServerName swank.local:80/g" /etc/httpd/conf/httpd.conf
+sudo sed -i "s/ServerName swank.local:80//g" /etc/apache2/httpd.conf
+sudo sh -c 'echo "ServerName swank.local:80" >> /etc/apache2/httpd.conf'
 
 # Add hosts entries (preventing duplicates by first removing them first if
 # they're already there).
@@ -18,19 +19,17 @@ sudo sed -i "s/#ServerName www.example.com:80/ServerName swank.local:80/g" /etc/
 #sudo echo "127.0.0.1 swank.local" >> /etc/hosts
 
 # Retrieve the composer dependencies.
-cd /var/www/swank/application
+cd /var/www/swank/
 sudo php composer.phar self-update
 sudo php composer.phar update --dev
 
 # Give Apache write permission to the folders it needs it for.
-sudo chown www-data:www-data /var/www/swank/application/protected/runtime
-sudo chown www-data:www-data /var/www/swank/application/assets
+sudo chown www-data:www-data /var/www/swank/protected/runtime
+sudo chown www-data:www-data /var/www/swank/public/assets
 
 # Copy the conf file to where Apache will find it.
-sudo cp /vagrant/swank-vhost.conf /etc/httpd/conf.d/
-
-# Adjust the mode settings on that conf file.
-sudo chmod 644 /etc/httpd/conf.d/swank-vhost.conf
+sudo cp /vagrant/swank-vhost.conf /etc/apache2/sites-available/
+sudo ln -s /etc/apache2/sites-available/swank-vhost.conf /etc/apache2/sites-enabled/swank-vhost.conf
 
 # Restart Apache.
 sudo apachectl restart
