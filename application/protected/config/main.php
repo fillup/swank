059498bbfa4f8cc6c069870b911a79dcb7aaf293
Phillip Shipley <phillip_shipley@sil.org>
2013-10-17 16:22:20 -0400

    removing yii-bootstrap extension since not used

--- application/protected/config/main.php
+++ application/protected/config/main.php
@@ -2,8 +2,6 @@
 
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
- 
-Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
@@ -31,9 +29,6 @@ return array(
     ),
     // application components
     'components' => array(
-        'bootstrap'=>array(
-            'class'=>'bootstrap.components.Bootstrap',
-        ),
         'user' => array(
             // enable cookie-based authentication
             'allowAutoLogin' => true,
