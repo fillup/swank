00f605a7cc0579a7cbc96f4353ecb6e91915bcf4
Phillip Shipley <phillip_shipley@sil.org>
2013-10-17 09:05:26 -0400

    vagrant configuration working now

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -9,27 +9,9 @@ sudo mount -t vboxsf -o uid=`id -u www-data`,gid=`id -g www-data` /var/www/swank
 # Make sure the timezone is set in php.ini.
 sudo sed -i".bak" "s/^\;date\.timezone.*$/date\.timezone = \"America\\/New_York\" /g" /etc/php5/apache2/php.ini
 
-# Change apache servername
-sudo sed -i "s/ServerName swank.local:80//g" /etc/apache2/httpd.conf
-sudo sh -c 'echo "ServerName swank.local:80" >> /etc/apache2/httpd.conf'
-
-# Add hosts entries (preventing duplicates by first removing them first if
-# they're already there).
-#sudo sed -i "s/127.0.0.1 swank.local//g" /etc/hosts
-#sudo echo "127.0.0.1 swank.local" >> /etc/hosts
-
-# Retrieve the composer dependencies.
-cd /var/www/swank/
-sudo php composer.phar self-update
-sudo php composer.phar update --dev
-
-# Give Apache write permission to the folders it needs it for.
-sudo chown www-data:www-data /var/www/swank/protected/runtime
-sudo chown www-data:www-data /var/www/swank/public/assets
-
 # Copy the conf file to where Apache will find it.
 sudo cp /vagrant/swank-vhost.conf /etc/apache2/sites-available/
-sudo ln -s /etc/apache2/sites-available/swank-vhost.conf /etc/apache2/sites-enabled/swank-vhost.conf
+sudo a2ensite swank-vhost.conf
 
 # Restart Apache.
-sudo apachectl restart
+sudo service apache2 restart
